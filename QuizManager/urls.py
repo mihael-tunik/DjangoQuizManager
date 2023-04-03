fe7c580b2f6a1fe942e51edaf502863ee7a139df
Mihael <mihael.8112@yahoo.com>
2023-04-03 11:36:04 +0300

    requirements.txt file added, in urls.py: re_path() instead of url()

--- QuizManager/urls.py
+++ QuizManager/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from Quizzes import views
-from django.conf.urls import url
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/quizzes/$', views.quiz_list),
-    url(r'^api/quizzes/(?P<pk>[0-9]+)$', views.quizzes_detail),
+    re_path(r'^api/quizzes/$', views.quiz_list),
+    re_path(r'^api/quizzes/(?P<pk>[0-9]+)$', views.quizzes_detail),
 ]
