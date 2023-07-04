8ac12f72f1d0c77005beed0a951b6cfc953bef3e
Mihael <mihael.8112@yahoo.com>
2023-07-04 10:32:09 +0300

    New backend app for JWT authentication

--- backend/django_backend/QuizManager/urls.py
+++ backend/django_backend/QuizManager/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 
 from Quizzes import views
 #from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users/', include('AuthJWT.api.urls')),
     path('api/quizzes/', views.quiz_list, name="quiz-list"),
     re_path(r'^api/quizzes/$', views.quiz_list),
     re_path(r'^api/quizzes/(?P<pk>[0-9]+)$', views.quizzes_detail),
