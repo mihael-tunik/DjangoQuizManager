3779d8cf66e12023b937a34dc518dfccffa22954
Mihael <mihael.8112@yahoo.com>
2023-07-07 15:31:05 +0300

    useParams(), useRef() in QuizCreateUpdate module

--- frontend/react_frontend/src/QuizCreateUpdate.js
+++ frontend/react_frontend/src/QuizCreateUpdate.js
@@ -1,93 +1,87 @@
-import React, { Component } from 'react'
+import React, { Component, useRef, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import QuizzesService from './QuizzesService'
 
 const quizzesService = new QuizzesService()
 
-class QuizCreateUpdate extends Component {
-  constructor (props) {
-    super(props)
+export default function QuizCreateUpdate(){
+    const QuizName = useRef(null);
+    const Description = useRef(null);
+    const JsonContent = useRef(null);
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  componentDidMount () {
-    const { match: { params } } = this.props
-    console.log(params)
-
-    if (params && params.pk) {
-      quizzesService.getQuiz(params.pk).then((c) => {
-        this.refs.quiz_name.value = c.quiz_name
-        this.refs.description.value = c.description
-        this.refs.json_content.value = c.json_content
-
-        // console.log(c)
+    const { pk } = useParams()
+      
+    useEffect(() => {
+         
+      if (pk) {
+        
+        quizzesService.getQuiz(pk).then((c) => {
+          QuizName.current.value = c.quiz_name
+          Description.current.value = c.description
+          JsonContent.current.value = c.json_content
+        })
+    
+      }
+    }, []);  
+    
+    function handleCreate () {
+      quizzesService.createQuiz(
+        {
+          quiz_name: QuizName.current.value, 
+          description: Description.current.value,
+          json_content: JsonContent.current.value
+        }
+      ).then((result) => {
+        alert('Quiz created!')
+      }).catch(() => {
+        alert('There was an error! Please re-check your form.')
       })
     }
-  }
-
-  handleCreate () {
-    quizzesService.createQuiz(
-      {
-        quiz_name: this.refs.quiz_name.value,
-        description: this.refs.description.value,
-        json_content: this.refs.json_content.value
+  
+    function handleUpdate (pk) {
+      quizzesService.updateQuiz(
+        {
+          pk,
+          quiz_name: QuizName.current.value, 
+          description: Description.current.value,
+          json_content: JsonContent.current.value
+        }
+      ).then((result) => {
+        console.log(result)
+        alert('Quiz updated!')
+      }).catch(() => {
+        alert('There was an error! Please re-check your form.')
+      })
+    }
+  
+    function handleSubmit (event) {  
+      if (pk) {
+        this.handleUpdate(pk)
+      } else {
+        this.handleCreate()
       }
-    ).then((result) => {
-      alert('Quiz created!')
-    }).catch(() => {
-      alert('There was an error! Please re-check your form.')
-    })
-  }
-
-  handleUpdate (pk) {
-    quizzesService.updateQuiz(
-      {
-        pk,
-        quiz_name: this.refs.quiz_name.value,
-        description: this.refs.description.value,
-        json_content: this.refs.json_content.value
-      }
-    ).then((result) => {
-      console.log(result)
-      alert('Quiz updated!')
-    }).catch(() => {
-      alert('There was an error! Please re-check your form.')
-    })
-  }
-
-  handleSubmit (event) {
-    const { match: { params } } = this.props
-
-    if (params && params.pk) {
-      this.handleUpdate(params.pk)
-    } else {
-      this.handleCreate()
+  
+      event.preventDefault()
     }
 
-    event.preventDefault()
-  }
-
-  render () {
     return (
-          <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
+      <form onSubmit={handleSubmit}>
+      <div className="form-group">
 
-              <label className="quiz-form-label">Quiz name:</label>
-              <textarea className="quiz-form-text-area" ref='quiz_name' />
+          <label className="quiz-form-label">Quiz name:</label>
+          <textarea className="quiz-form-text-area" ref={QuizName} />
 
-              <label className="quiz-form-label">Description:</label>
-              <textarea className="quiz-form-text-area" ref='description' ></textarea>
+          <label className="quiz-form-label">Description:</label>
+          <textarea className="quiz-form-text-area" ref={Description} ></textarea>
 
-              <label className="quiz-form-label">Quiz[JSON]:</label>
-              <textarea className="quiz-form-text-area" ref='json_content' ></textarea>
-              
-              <input className="btn btn-primary" type="submit" value="Submit" />
-                           
-            </div>
-                      
-          </form>
+          <label className="quiz-form-label">Quiz[JSON]:</label>
+          <textarea className="quiz-form-text-area" ref={JsonContent} ></textarea>
+          
+          <input className="btn btn-primary" type="submit" value="Submit" />
+                       
+        </div>
+                  
+      </form>
     )
-  }
-}
 
-export default QuizCreateUpdate
+}
