e7e3b4bc716f8946d712b7e3d00c5b2e7b1d5b23
Mihael <mihael.8112@yahoo.com>
2023-07-07 14:55:40 +0300

    Migrate to functional components and useRef() in QuizAppend module

--- frontend/react_frontend/src/QuizAppend.js
+++ frontend/react_frontend/src/QuizAppend.js
@@ -1,73 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component, useRef} from 'react'
+import { useParams } from 'react-router-dom'
 import QuizzesService from './QuizzesService'
 
 const quizzesService = new QuizzesService()
 
-class QuizAppend extends Component {
-  constructor (props) {
-    super(props)
+export default function QuizAppend(){
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  componentDidMount () {
-    const { match: { params } } = this.props
-    console.log(params)
-  }
-
-  handleUpdate (pk) {
-        
-    quizzesService.getQuiz(pk).then((c) => {
-        
-        const updated_json_content = JSON.parse(c.json_content)
-    
-        console.log(updated_json_content)
-        updated_json_content[this.refs.l_value.value] = this.refs.r_value.value
-        console.log(updated_json_content)
-    
-        quizzesService.updateQuiz(
-        {
-          pk,
-          quiz_name: c.quiz_name,
-          description: c.description,
-          json_content: JSON.stringify(updated_json_content)
-         }
-        ).then((result) => {
-          console.log(result)
-          alert('Quiz updated!')
-        }).catch(() => {
-          alert('There was an error! Please re-check your form.')
-        })
-    })
-
-  }
-
-  handleSubmit (event) {
-    const { match: { params } } = this.props
-
-    if (params && params.pk) {
-      this.handleUpdate(params.pk)
-    }
-    event.preventDefault()
-  }
-
-  render () {
-    return (
-          <form onSubmit={this.handleSubmit}>
+    const lValue = useRef(null);
+    const rValue = useRef(null);
+      
+    function handleUpdate (pk) {
+      
+      if(lValue && rValue){
+      
+        quizzesService.getQuiz(pk).then((c) => {
           
-          <div className="form-group">
-              <label className="quiz-form-label">Add:</label>
-              
-              <input className="quiz-form-new-data" type="text" ref='l_value' />
-              <input className="quiz-form-new-data" type="text" ref='r_value' />
-              
-              <input className="btn btn-primary" type="submit" value="Append" />
-                 
-          </div>
-                      
-          </form>
-    )
-  }
-}
+          const updated_json_content = JSON.parse(c.json_content)
+      
+          //console.log(updated_json_content)
+          updated_json_content[lValue.current.value] = rValue.current.value
+          
+          //console.log(updated_json_content)
+      
+          quizzesService.updateQuiz(
+          {
+            pk,
+            quiz_name: c.quiz_name,
+            description: c.description,
+            json_content: JSON.stringify(updated_json_content)
+           }
+          ).then((result) => {
+            console.log(result)
+            alert('Quiz updated!')
+          }).catch(() => {
+            alert('There was an error! Please re-check your form.')
+          })
+        })
+      
+      }
+  
+    }
+  
+    function handleSubmit (event) {
+      const { pk } = useParams()
+  
+      if (pk) {
+        handleUpdate(pk)
+      }
+      event.preventDefault()
+    }
 
-export default QuizAppend
+    return (
+      <form onSubmit={handleSubmit}>
+      
+      <div className="form-group">
+          <label className="quiz-form-label">Add:</label>
+          
+          <input className="quiz-form-new-data" type="text" ref={lValue} />
+          <input className="quiz-form-new-data" type="text" ref={rValue} />
+          
+          <input className="btn btn-primary" type="submit" value="Append" />
+             
+      </div>
+                  
+      </form>
+    )
+}
